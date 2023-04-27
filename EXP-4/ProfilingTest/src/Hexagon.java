3a01fb8df4e1dfed0a0934794db705b56e363c22
Arman Mohammadi <rman.mo2000@gmail.com>
2023-04-27 16:04:00 +0330

    second code

--- EXP-4/ProfilingTest/src/Hexagon.java
+++ EXP-4/ProfilingTest/src/Hexagon.java
@@ -7,37 +7,34 @@ public class Hexagon {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int k = scanner.nextInt();
-        char[][] matrix = new char[2 * n + 1][2 * n + m];
+        String lines;
         int i = n;
-        int row = 0;
         while (i >= 0) {
+            lines = "";
             for (int j = 0; j < 2 * n + m; j++) {
                 if (j < i || 2 * n + m - j <= i)
-                    matrix[row][j] = ' ';
+                    lines = lines + ' ';
                 else
-                    matrix[row][j] = '*';
+                    lines = lines + '*';
             }
+            for(int a = 1; a <= k; a++)
+                System.out.print(lines);
+            System.out.print("\n");
             i--;
-            row++;
         }
         i += 2;
-        while (i <= n) {
+        while (i <= n){
+            lines = "";
             for (int j = 0; j < 2 * n + m; j++) {
                 if (j < i || 2 * n + m - j <= i)
-                    matrix[row][j] = ' ';
+                    lines = lines + ' ';
                 else
-                    matrix[row][j] = '*';
+                    lines = lines + '*';
             }
-            i++;
-            row++;
-        }
-        for (int l = 0; l < 2 * n + 1; l++) {
-            for (int a = 1; a <= k; a++) {
-                for (int j = 0; j < 2 * n + m; j++) {
-                    System.out.print(matrix[l][j]);
-                }
-            }
+            for(int a = 1; a <= k; a++)
+                System.out.print(lines);
             System.out.print("\n");
+            i++;
         }
     }
 }
